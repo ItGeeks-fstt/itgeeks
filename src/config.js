9677a917501a77bed7439cdced67772d5c449011
The-Shinigami <mohamedbarbarr@gmail.com>
2022-09-25 20:11:15 +0100

    change firebase settings

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const APY_URL='https://it-geeks-fstt-default-rtdb.europe-west1.firebasedatabase.app/';
+export const APY_URL='https://it-geeks-de1b9-default-rtdb.europe-west1.firebasedatabase.app/';
