9229192b43fb195357f8bc75b0865e0589bb7ae1
Sohaib Manah <souhaibemanah@gmail.com>
2023-03-11 23:14:15 +0100

    update footer

--- src/components/footers/MiniCenteredFooter.js
+++ src/components/footers/MiniCenteredFooter.js
@@ -1,20 +1,20 @@
 import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
-import {Container as ContainerBase } from "components/misc/Layouts.js"
+import { Container as ContainerBase } from "components/misc/Layouts.js";
 import logo from "../../images/logo.jpg";
 import { Link } from "react-router-dom";
 
-const Container = tw(ContainerBase)`bg-primary-900 text-gray-100 -mx-8 -mb-8`
+const Container = tw(ContainerBase)`bg-primary-900 text-gray-100 -mx-8 -mb-8`;
 const Content = tw.div`max-w-screen-xl mx-auto py-5 lg:py-6`;
 
-const Row = tw.div`flex items-center justify-center flex-col px-8`
+const Row = tw.div`flex items-center justify-center flex-col px-8`;
 
 const LogoContainer = tw.div`flex items-center justify-center md:justify-start`;
 const LogoImg = tw.img`w-8`;
 const LogoText = tw.h5`ml-2 text-2xl font-black tracking-wider`;
 
-const LinksContainer = tw.div`mt-8 font-medium flex flex-wrap justify-center items-center flex-col sm:flex-row`
+const LinksContainer = tw.div`mt-8 font-medium flex flex-wrap justify-center items-center flex-col sm:flex-row`;
 const Linkspan = tw.span`border-b-2 border-transparent hocus:text-gray-300 hocus:border-gray-300 pb-1 transition duration-300 mt-2 mx-4`;
 
 const SocialLinksContainer = tw.div`mt-10`;
@@ -25,8 +25,8 @@ const SocialLink = styled.a`
   }
 `;
 
-const CopyrightText = tw.p`text-center mt-10 font-medium tracking-wide text-sm text-gray-600`
-export default ({links,socialLinks}) => {
+const CopyrightText = tw.p`text-center mt-10 font-medium tracking-wide text-sm text-gray-600`;
+export default ({ links, socialLinks }) => {
   return (
     <Container>
       <Content>
@@ -36,26 +36,48 @@ export default ({links,socialLinks}) => {
             <LogoText>It Geeks</LogoText>
           </LogoContainer>
           <LinksContainer>
-              {
-                links.map((link,key )=> {
-                 return <Link key={key} to={link.link}> <Linkspan >{link.name}</Linkspan></Link>
-                })
-              }
+            {links.map((link, key) => {
+              return (
+                <Link key={key} to={link.link}>
+                  {" "}
+                  <Linkspan>{link.name}</Linkspan>
+                </Link>
+              );
+            })}
           </LinksContainer>
           <SocialLinksContainer>
-          {
-                socialLinks.map((socialLink,key) => {
-                  return  <SocialLink key={key} rel="noreferrer"  target="_blank" href={socialLink.link}>
+            {socialLinks.map((socialLink, key) => {
+              return (
+                <SocialLink
+                  key={key}
+                  rel="noreferrer"
+                  target="_blank"
+                  href={socialLink.link}
+                >
                   {socialLink.icon}
-               </SocialLink>
-                })
-              }       
+                </SocialLink>
+              );
+            })}
           </SocialLinksContainer>
           <CopyrightText>
-            &copy; Copyright 2022, Itgeeks Inc. All Rights Reserved.
-            This web site is made by our team  <a href="https://www.instagram.com/it_geeks_fstt" rel="noreferrer"  target="_blank">Itgeeks</a> 
-             &nbsp; if you face any problem, please contact us. &nbsp; 
-             <a href = "mailto: support@itgeeks.social">support@itgeeks.social</a>
+            &copy; Copyright 2022, Itgeeks Inc. All Rights Reserved. This web
+            site is made by our team{" "}
+            <a
+              href="https://www.instagram.com/it_geeks_fstt"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Itgeeks
+            </a>
+            &nbsp; if you face any problem, please contact the developer. &nbsp;
+            <a
+              href="https://developer.itgeeks.social/"
+              rel="noreferrer"
+              target="_blank"
+              style={{ color: "white" }}
+            >
+              click me
+            </a>
           </CopyrightText>
         </Row>
       </Content>
